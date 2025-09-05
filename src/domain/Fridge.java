4364adc149a662ffd7e569fa268a2158340c8c7e
NaGihyeon <nesd0957@gmail.com>
2025-09-05 16:45:00 +0900

    temp

--- src/domain/Fridge.java
+++ src/domain/Fridge.java
@@ -1,5 +1,13 @@
 package domain;
 
+import java.util.HashMap;
+
 public class Fridge {
 
+	private HashMap<Integer, Integer> foodList; //냉장고에 들어간 음식 목록
+	private int waterCnt; //냉장고 물 개수
+	
+	
+	
+	
 }
--- src/service/FridgeService.java
+++ src/service/FridgeService.java
@@ -1,5 +1,55 @@
 package service;
 
+import domain.HomeFood;
+
 public class FridgeService {
+	
+	
+	/*
+	 * 냉장고에 음식을 넣는 함수
+	 */
+	public void putFood(String name, int count) {
+		new HomeFood();
+		
+		
+		
+	}
+	
+	/*
+	 * 냉장고의 음식 목록을 보여주는 함수
+	 */
+	public void foodList() {
+		
+		
+	}
+	
+	/*
+	 * 음식 수량 체크 함수
+	 * 남은 음식 수량이 최소 필요 개수보다 낮을 시 경고를 출력
+	 */
+	public void checkFood(String name) {
+		
+		
+	}
+	
+	/*
+	 * 음식을 먹는 함수
+	 * 음식을 먹었을 시 해당 음식의 수량을 감소시킨다.
+	 */
+	public void eatFood(String name){
+		
+		
+	}
+	
+	/*
+	 * 음식 삭제 함수
+	 * 음식을 삭제하면 해당 음식의 수량을 0으로 바꾼다.
+	 */
+	public void deleteFood(String name) {
+		
+		
+	}
+	
+	
 
 }
