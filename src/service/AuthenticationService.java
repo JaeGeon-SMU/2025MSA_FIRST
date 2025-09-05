87acd0f41d8904c3d82873034ddfe87742e0c6dd
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2025-09-05 21:49:41 +0900

    로그인 구현 및 enum value값 추가

--- src/service/AuthenticationService.java
+++ src/service/AuthenticationService.java
@@ -1,8 +1,25 @@
 package service;
 
+import domain.User;
+import repo.UserRepo;
+
 public class AuthenticationService {
+	UserRepo userRepo;
+	
+	public AuthenticationService() {
+		this.userRepo = new UserRepo();
+	}
+
 	void logout() {
 		System.out.println("로그아웃 되었습니다.");
 		System.exit(0);
+	}
+	
+	User login(String id,String password) {
+	    User user = userRepo.findById(id);
+	    if (user != null && password.equals(user.getPassword())) {
+	        return user;
+	    }
+	    return null;
 	}
 }
