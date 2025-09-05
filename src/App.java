3e949396ee96ddad261d7f7f95a0c52b49db296c
NaGihyeon <nesd0957@gmail.com>
2025-09-05 17:43:42 +0900

    Feat: FridgeService 틀 잡기

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
+import service.FridgeService;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//FridgeService f = new FridgeService();
+		//f.deleteFood();
 	}
 
 }
--- src/domain/Food.java
+++ src/domain/Food.java
@@ -1,9 +1,11 @@
 package domain;
 
 public abstract class Food {
-	int id;
-	String name;
-	int calorie;
-	int protein;
-	Allergy allergy;
+	int id; //음식 id
+	String name; //음식 이름
+	int calorie; //칼로리
+	int protein; //단백질
+	Allergy allergy; //알레르기 정보
+	
+	
 }
