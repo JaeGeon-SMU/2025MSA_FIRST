2e2480fef2845084ff6777ce4b96885929674000
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2025-09-06 20:33:51 +0900

    user 멤버변수 변경

--- src/domain/DailyGoalInfo.java
+++ src/domain/DailyGoalInfo.java
@@ -3,5 +3,11 @@ package domain;
 public class DailyGoalInfo {
 	private double currentWeight;
 	private double targetWeight;
+	private int targetProtein;
+    private int targetCalories;
+    private int minMeal;
+    private int age;
+    private double height;
+    private int targetWater;
 
 }
