e91383cf4dfa853dc5f34115997a469459821b45
NaGihyeon <nesd0957@gmail.com>
2025-09-06 11:27:08 +0900

    feat: FridgeService 함수 구현

--- src/domain/HomeFood.java
+++ src/domain/HomeFood.java
@@ -1,10 +1,21 @@
 package domain;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class HomeFood extends Food {
 	private LocalDate expireDate; //유통기한
 	private int reorderPoint; //최소수량
+	
+	public HomeFood(String name) {
+		this(name, 0, 0, null, null, 0);		
+	}
+	
+	public HomeFood(String name, int calorie, int protein, List<Allergy> allergy, LocalDate expireDate, int reorderPoint) {
+		super(name, calorie, protein, allergy);
+		this.expireDate = expireDate;
+		this.reorderPoint = reorderPoint;
+	}
 	
 	
 	public int getReorderPoint() {
@@ -12,5 +23,9 @@ public class HomeFood extends Food {
 	}
 	
 	
+	@Override
+	public String toString() {
+		return super.toString() + ", exp: " + expireDate;
+	}
 	
 }
