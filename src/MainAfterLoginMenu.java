6b3e48be3c13d454f0f5859d9e2a9c8ef5d36bdd
NaGihyeon <nesd0957@gmail.com>
2025-09-08 17:30:13 +0900

    feat EatingOutMenu 추가

--- src/MainAfterLoginMenu.java
+++ src/MainAfterLoginMenu.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 import domain.User;
 import service.ChatGptSummaryService;
+import service.EatingOutService;
 import service.FridgeService;
 import service.UserService;
 
@@ -11,12 +12,17 @@ public class MainAfterLoginMenu {
     private final ChatGptSummaryService gptService;
     private final Scanner sc;
     private final FridgeService fridgeService;
+    private final EatingOutService eatingOutService;
     private String quotes;
+    
+    private final FridgeMenu fridgeMenu;
+    private final EatingOutMenu eatingOutMenu;
 
     public MainAfterLoginMenu(User user, Scanner sc) {
         this.user = user;
         this.userService = new UserService();
         this.fridgeService = new FridgeService(user);
+        this.eatingOutService = new EatingOutService(user);        
         this.gptService = new ChatGptSummaryService();
         //시연할 때 키 값 추가!
         if(!gptService.getApiKey().equals("")) {
@@ -25,6 +31,9 @@ public class MainAfterLoginMenu {
         	this.quotes = "";
         }
         this.sc = sc;
+        
+        this.fridgeMenu = new FridgeMenu(fridgeService, userService, user, sc);
+        this.eatingOutMenu = new EatingOutMenu(eatingOutService, userService, user, sc);
     }
 
     public void run() {
@@ -47,9 +56,10 @@ public class MainAfterLoginMenu {
             System.out.println("3. 회원 정보 보기");
             System.out.println("4. 오늘 먹은 음식 보기");
             System.out.println("5. 냉장고 관리");
-            System.out.println("6. 이번 달 통계 보기");
-            System.out.println("7. AI 요약");
-            System.out.println("8. 로그아웃 / 종료");       // ★ 번호 한 칸 뒤로
+            System.out.println("6. 외식 메뉴 열기");            
+            System.out.println("7. 이번 달 통계 보기");
+            System.out.println("8. AI 요약");
+            System.out.println("9. 로그아웃 / 종료");       // ★ 번호 한 칸 뒤로
             System.out.print("선택: ");
 
             String in = sc.nextLine().trim();
@@ -80,15 +90,18 @@ public class MainAfterLoginMenu {
                     break;
                 }
                 case 5: {
-                	  FridgeMenu fridgeMenu = new FridgeMenu(fridgeService, userService, user, sc);
                     fridgeMenu.start();
                     break;
                 }
                 case 6: {
+                		eatingOutMenu.start();
+                		break;
+                }
+                case 7: {
                     userService.checkMonthlyGoal(user);
                     break;
                 }
-                case 7: {
+                case 8: {
                 	if(!gptService.getApiKey().equals("")) {
                 		System.out.println("\t...요약중...\n");
                 		System.out.println(gptService.askGptWithUserPrompt(user).replace("\"", "\\\"").replace("\n", "\\n") + "\"}");
@@ -97,7 +110,7 @@ public class MainAfterLoginMenu {
                     }
                 	break;
                 }
-                case 8: {
+                case 9: {
                     System.out.println("로그아웃합니다. 안녕!");
                     return;
                 }
